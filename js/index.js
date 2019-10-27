6674880ec17f884c76620c49bc18123311246992
Eddboy01 <jacksonedward29@gmail.com>
2019-10-27 09:49:41 -0500

    Added Initial Files

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const navbtn = document.getElementById("navbtn");
+function openNav() {
+  document.getElementById("navigation").classList.add("active");
+}
+
+function closeNav() {
+  document.getElementById("navigation").classList.remove("active");
+}
